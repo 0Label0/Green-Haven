1f57f75004a355fd58522c758e5bd7fe2ac9bf8a
Héctor Ventura Álvarez <hectorvenalvarez@gmail.com>
2024-06-05 03:16:28 +0200

    - Ahora el formulario de php funciona

--- scripts/process_from.php
+++ scripts/process_from.php
@@ -12,25 +12,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     
     if (empty($name)) {
         $errors[] = 'El nombre está vacío';
-    } else if(!preg_match("/^[a-zA-ZáéíóúÁÉÍÓÚ\s]+$/", $name)){
+    } else if(!preg_match("/[a-zA-ZáéíóúÁÉÍÓÚ\s]/", $name)){
         $errors[] = 'Debe escribir el nombre sólo con letras del alfabeto Español';
     } else if (empty($mail)) {
         $errors[] = 'El mail está vacío';
-    } else if (!preg_match($mail, FILTER_VALIDATE_EMAIL)) {
+    } else if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
         $errors[] = 'Mail no válido';
     } else if (empty($tel)) {
         $errors[] = 'El teléfono está vacío';
-    } else if (!preg_match("/^[0-9]{4}-[0-9]{4}-[0-9]{5}$/", $tel)) {
+    } else if (!preg_match("/[0-9]{4}-[0-9]{4}-[0-9]{5}/", $tel)) {
         $errors[] = 'El número de teléfono es inválido';
     }
 
     if (empty($errors)) {
+        // Enviar los datos al server
         echo 'formulario enviado correctamente';
-    } else {
+    }else {
         foreach ($error as $errors) {
             echo "<p>$error</p>";
+
+
         }
     }
-
 
 }
